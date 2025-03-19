76a1a8cfc289366623a1325f040f4c7093cf2ac5
Aksel Wester <aksel@wester.co>
2025-03-19 16:58:28 +0100

    Complete Part 1

--- part-1/src/main.rs
+++ part-1/src/main.rs
@@ -1,3 +1,9 @@
+use std::thread;
+
 fn main() {
-    todo!()
+    let thread_join_handle = thread::spawn(move || {
+        println!("Hello from thread!")
+    });
+    println!("Hello from main thread!");
+    let _ = thread_join_handle.join();
 }
